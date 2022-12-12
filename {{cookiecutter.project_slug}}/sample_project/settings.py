39097ef4407d679f22aee70c07c8c59f72dcf32c
Bin <bin.ngoctu@gmail.com>
2022-12-12 22:06:21 +0700

    Debug Libs

--- {{cookiecutter.project_slug}}/sample_project/settings.py
+++ {{cookiecutter.project_slug}}/sample_project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -30,17 +29,18 @@ ALLOWED_HOSTS = [
     "localhost",
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
+    "{{ cookiecutter.app_slug }}",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "{{ cookiecutter.app_slug }}",
+    "debug_toolbar",
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "sample_project.urls"
@@ -73,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sample_project.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -83,7 +83,6 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -103,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -117,8 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
